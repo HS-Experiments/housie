7604b3c48179c5d5c718fd9d8c001c0e01dc98b9
Harshad <harshad.satra2@gmail.com>
2020-05-21 15:04:05 +0530

    layout 1

---
+++ housiee_controllr.php
@@ -0,0 +1,35 @@
+{
+  "array": {
+    "1":true,
+    "2":true,
+    "3":true
+  },
+  "boolean": true,
+  "color": "gold",
+  "null": null,
+  "number": 123,
+  "object": {
+    "a": "b",
+    "c": "d"
+  },
+  "string": "Hello World",
+  "prizes":{
+    "1":{
+      "name":"Quick 5",
+      "winner":null,
+      "amount":50
+    }
+  },
+  "particpents":{
+    "1":{
+      "name":"Harshad",
+      "tickits":2
+    }
+  },
+  "rules":[
+    "abc","pqr" 
+  ],
+  "other_info":[
+    "abc","pqr" 
+  ]
+}
